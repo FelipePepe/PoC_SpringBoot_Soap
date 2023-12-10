f5fc7777bb74cacc08e0b79323fd4069355fdabd
Felipe Pérez <felipepepe@gmail.com>
2023-12-10 15:06:50 +0100

    PoC Spring Boot client - Web Services Soap

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PoC_SpringBoot_Soap
+
+URL: <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    Soap: http://www.dneonline.com/calculator.asmx
--- src/main/java/com/soap/SpringBootSoapApplication.java
+++ src/main/java/com/soap/SpringBootSoapApplication.java
@@ -1,13 +1,47 @@
 package com.soap;
 
+import com.soap.client.SoapClient;
+import com.soap.wsdl.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootSoapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSoapApplication.class, args);
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootSoapApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSoapApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(SoapClient soapClient) {
+
+        int numberA = 200;
+        int numberB = 15;
+
+        try {
+            AddResponse addResponse = soapClient.getAddResponse(numberA, numberB);
+            LOGGER.info("El resultado de {} + {} = {}", numberA, numberB, addResponse.getAddResult());
+
+            SubtractResponse subtractResponse = soapClient.getSubtractResponse(numberA, numberB);
+            LOGGER.info("El resultado de {} - {} = {}", numberA, numberB, subtractResponse.getSubtractResult());
+
+            MultiplyResponse multiplyResponse = soapClient.getMultiplyResponse(numberA, numberB);
+            LOGGER.info("El resultado de {} x {} = {}", numberA, numberB, multiplyResponse.getMultiplyResult());
+
+            DivideResponse divideResponse = soapClient.getDivideResponse(numberA, numberB);
+            LOGGER.info("El resultado de {} / {} = {}", numberA, numberB, divideResponse.getDivideResult());
+
+        } catch (Exception ex) {
+            LOGGER.error(ex.toString());
+        }
+
+        return null;
+    }
 
 }
